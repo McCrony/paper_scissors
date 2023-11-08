d5cc9778bf082ea1a8e2b6fe38048d5c8caff170
McCrony <askmccrony@gmail.com>
2023-11-08 04:39:24 +0000

    Create getComputerChoice, playRound, and game functions

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
+
+const userScore = 0;
+const computerScore =0;
+const rockVariations = ["Roc"]
+const computerSelection = getComputerChoice();
+
+let playerSelection = prompt("Enter your choice");
+
+alert(computerSelection + " " + playerSelection);
+
+
 function getComputerChoice(){
     const compOptions = ["Rock", "Paper" , "Scissors"];
     const compChoice = Math.floor(Math.random() * compOptions.length);
     return compOptions[compChoice]
 }
 
-let res = getComputerChoice();
-console.log(res);
+function playRound(playerSelection, computerSelection){
+
+    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        console.log("You Win! Rock beats Scissors" + "Com: " + computerSelection);
+        
+    } else if (playerSelection === "Rock" && computerSelection === "Rock"){
+       console.log("You lose! Rock beats Scissors")
+    } else{
+        console.log("Nothing happens");
+    }
+}
+
+
+function game(){
+    playRound(playerSelection, computerSelection);
+}
+
+for(let a = 0; a <=4; a++){
+    game();
+}
