27960bbbec1bef5a878b99149261312443e0b891
McCrony <askmccrony@gmail.com>
2023-11-09 09:24:02 +0000

    Create getResults function to declare the final results of game

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
-const playerScore = 0;
-const computerScore =0;
+let playerScore = 0;
+let computerScore =0;
 const rockVariations = ["Rock", "rock", "ROCK", "RoCK", "rocK", "rOck"];
 const scissorsVariatons = ["Scissors", "scissors", "SCISSORS"];
 const paperVariations = ["Paper", "paper", "PAPER"];
-const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice();
 
+let playerSelection = prompt("Enter your choice");
 
 
 game();
+
+
 
 function getComputerChoice(){
     const compOptions = ["Rock", "Paper" , "Scissors"];
@@ -17,42 +20,49 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-
-    let playerSelection = prompt("Enter your choice");
+   
 
     if (rockVariations.includes(playerSelection) && computerSelection === "Scissors") {
-        alert("You Win! Rock beats Scissors" + "Com: " + computerSelection);   
-        alert( "Your score: " + playerScore++);
+        alert("You Win! Rock beats Scissors" + "Com: " + computerSelection);
+        alert("Your score: " + playerScore++ + "Com Score: " + computerScore++);  
     }else if(computerSelection === "Rock" && scissorsVariatons.includes(playerSelection)){
         alert("Computer wins! Rock beats Scissors " + "Com: " + computerSelection);
-        computerScore++;
+        alert("Your score: " + playerScore + "Com Score: " + computerScore++);
     } 
     
     else if (scissorsVariatons.includes(playerSelection) && computerSelection === "Paper"){
        alert("You Win! Scissors beats Paper" + "Com: " + computerSelection);
-       playerScore++;
+       alert("Your score: " + playerScore++ + "Com Score: " + computerScore);  
     }else if(computerSelection === "Scissors" && paperVariations.includes(playerSelection)){
         alert("Computer wins! Scissors beats Paper " + " Com: " + computerSelection);
-        computerScore++;
+        alert("Your score: " + playerScore++ + "Com Score: " + computerScore);  
     } 
     
     else if(paperVariations.includes(playerSelection) && computerSelection === "Rock"){
         alert("You Win! Paper beats Rock" + "Com: " + computerSelection);
-        playerScore++;
+        alert("Your score: " + playerScore++ + "Com Score: " + computerScore);  
     }else if(computerSelection === "Paper" && rockVariations.includes(playerSelection)){
         alert("Computer wins! Paper beats Rock " + "Com: " + computerSelection);
-        computerScore++;
+        alert("Your score: " + playerScore++ + "Com Score: " + computerScore);  
     }
 
     else{
         alert("It's a fair game" + "Com: " + computerSelection);
+        alert("Your score: " + playerScore++ + "Com Score: " + computerScore);  
     }
-
-    alert("Your score: " + playerScore + " : " + "Computer Score: " + computerScore);
+    // alert("Your score: " + playerScore + " : " + "Computer Score: " + computerScore);
 
 }
 
 
 function game(){
     playRound(playerSelection, computerSelection);
+}
+
+function getResults(){
+    if(playerScore>computerScore){
+        alert("You won the game with " + playerScore + "Score")
+    }else{
+        alert("You lose! ")
+    }
 }
